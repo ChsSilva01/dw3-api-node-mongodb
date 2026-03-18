fc9f7408726ac5de8b60a159fb51836b6de47ac5
Carlos Henrique <105667538+ChsSilva01@users.noreply.github.com>
2026-03-17 22:07:26 -0300

    @ChsSilva01

---
+++ 17.03.2026/WEBIII.md
@@ -0,0 +1,46 @@
+# WEBIII
+
+
+
+##### > Aula 04: Autenticação na API com JWT (json web token)
+
+
+
+JWT
+
+*// é um método para verificar a identidade de usuários e proteger o acesso a sistemas*
+
+
+
+
+
+* Processo de cadastro de um usuário
+
+Cliente -----------> /Users ----------> {API} ------> BD - user(Coleções)
+
+&#x09;req. (POST) 
+
+
+
+* Processo de login (jwt)
+
+
+
+&#x09;			   (TOKEN)
+
+&#x09;			<------------				
+
+Cliente ----------> /Auth -----------> {API} -------> BD 
+
+&#x09;req. (POST)
+
+
+
+*// Bearer token*
+
+*(token ao portador)*
+
+*Armazenando no navegador*
+
+
+
---
+++ 17.03.2026/aula-04-autenticacao-na-api/controllers/userController.js
@@ -0,0 +1,16 @@
+// Importando o service
+import userService from "../services/userService.js";
+
+// Função para CADASTRAR um usuário
+const createUser = async (req, res) => {
+    try {
+        const {name, email, password} = req.body
+        await userService.Create(name, email, password);
+        res.status(201).json({message: 'Usuário cadastrado com sucesso! '})
+        // Cod. 201 (CREATED)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Não foi possível cadastrar o usuário. Erro interno do servidor.'});
+    }
+}
+export default { createUser }
